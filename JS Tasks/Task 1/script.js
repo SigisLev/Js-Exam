6d00b22ef2827cf262d63fffb84dcbbf10731af9
SigisLev <sigitaslev@gmail.com>
2022-08-31 19:31:26 +0300

    First task completed

--- JS Tasks/Task 1/script.js
+++ JS Tasks/Task 1/script.js
@@ -8,3 +8,20 @@ pamatyti jo pateikto svorio kovertavimą į:
 Pastaba: atvaizdavimas turi būti matomas pateikus formą ir pateikiamas
 <div id="output"></div> viduje, bei turi turėti bent minimalų stilių;
 ------------------------------------------------------------------- */
+document.querySelector('form').addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const search = document.getElementById('search');
+  const resultContainer = document.getElementById('output');
+  resultContainer.className = 'output-style';
+
+  const lbOutput = document.createElement('div');
+  const gramsOutput = document.createElement('div');
+  const ozOutput = document.createElement('div');
+
+  resultContainer.append(lbOutput, gramsOutput, ozOutput);
+
+  lbOutput.textContent = 'Weight in lb: ' + search.value * 2.2046;
+  gramsOutput.textContent = 'Weight in grams: ' + search.value / 0.001;
+  ozOutput.textContent = 'Weight in oz: ' + search.value * 35.274;
+});
