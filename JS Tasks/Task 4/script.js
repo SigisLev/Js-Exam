6c32706ae0a4d6c231ef0c7d80b38d649db20569
SigisLev <sigitaslev@gmail.com>
2022-08-31 20:08:15 +0300

    Fourth task completed

--- JS Tasks/Task 4/script.js
+++ JS Tasks/Task 4/script.js
@@ -10,3 +10,30 @@ turėti bent minimalų stilių ir būti responsive;
 -------------------------------------------------------------------------- */
 
 const ENDPOINT = 'cars.json';
+
+fetch('./cars.json')
+  .then((response) => response.json())
+  .then((data) => {
+    renderCars(data);
+  });
+
+function renderCars(cars) {
+  const outputContainer = document.getElementById('output');
+  const carsContainer = document.createElement('div');
+  carsContainer.className = 'cars-container';
+
+  cars.forEach((car) => {
+    const carCard = document.createElement('div');
+    carCard.className = 'card';
+
+    const carBrand = document.createElement('h3');
+    const carModel = document.createElement('h5');
+
+    carBrand.textContent = car.brand;
+    carModel.textContent = car.models;
+
+    outputContainer.append(carsContainer);
+    carsContainer.append(carCard);
+    carCard.append(carBrand, carModel);
+  });
+}
